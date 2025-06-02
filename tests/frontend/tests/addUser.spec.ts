5958f872374660f407082cab3944afbc31e409a9
pk <pavel_kohout@centrum.cz>
2025-06-02 16:23:45 +0200

    clean addUSerSpec

--- tests/frontend/tests/addUser.spec.ts
+++ tests/frontend/tests/addUser.spec.ts
@@ -1,6 +1,4 @@
-import { expect } from "@playwright/test";
 import { test } from "../fixtures/frontend-fixtures";
-import { UserManagement } from "../pages/UserManagement.page";
 import { User } from "../data/User.data";
 
 test.only("should add a user to an empty db", async ({
